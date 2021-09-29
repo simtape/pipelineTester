cfc318f2e8795d8c922fb3c98f424aba888ef542
simtape <s.cassetta1@studenti.unimol.it>
2021-09-29 17:33:54 +0200

    splitted project in a macro module containing two modules- parser(it contains the pipeline parser) and repo_cloner(it will contain classes to clone a github repo)

---
+++ parser/src/main/java/it/unimol/com/app/Main.java
@@ -0,0 +1,10 @@
+package it.unimol.com.app;
+
+public class Main {
+
+    public static void main(String args[]) {
+        PipeLineParser pipeLineParser = new PipeLineParser();
+        pipeLineParser.parse();
+        //System.out.println(pipeLineParser.getOrder().getOrderNo());
+    }
+}
---
+++ parser/src/main/java/it/unimol/com/engine/Parser.java
@@ -0,0 +1,7 @@
+package it.unimol.com.engine;
+
+public interface Parser {
+    //capire quali jobs ha la pipeline
+    void parse();
+
+}
