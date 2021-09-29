65fcb77620435890613abf23f2bca8b6a31cea24
simtape <s.cassetta1@studenti.unimol.it>
2021-09-29 18:15:07 +0200

    added repo cloner

---
+++ repo_cloner/src/main/java/it/unimol/com/GitCloner.java
@@ -0,0 +1,44 @@
+package it.unimol.com;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class GitCloner {
+
+    private static GitCloner gitCloner = new GitCloner();
+
+    public static GitCloner getGitCloner() {
+        return gitCloner;
+    }
+
+    private GitCloner() {
+    }
+
+    private void downloadAndClone(String uri/*, String directory*/) throws InvalidRemoteException, TransportException, GitAPIException {
+/*        Git.cloneRepository()
+                .setURI(uri)
+                .setDirectory(new File(directory))
+                .call();*/
+        Git.cloneRepository()
+                .setURI(uri)
+                .call();
+    }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert uri of repository to download: ");
+        String uri = sc.nextLine();
+
+        try {
+            downloadAndClone(uri);
+        } catch (Exception e) {
+
+        }
+    }
+}
---
+++ repo_cloner/src/main/java/it/unimol/com/Main.java
@@ -0,0 +1,8 @@
+package it.unimol.com;
+
+public class Main {
+    public static void main(String args[]) {
+        GitCloner.getGitCloner().run();
+    }
+
+}
